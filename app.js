e110c1a644714a5f29c3237d1102a21b97c08e6f
SaileshK1103 <sailesh.karki@metropolia.fi>
2025-11-10 16:20:33 +0200

    Step4: Update routes as /api/.

--- app.js
+++ app.js
@@ -7,9 +7,9 @@ const userRouter = require("./routes/userRouter.js");
 app.use(express.json());
 
 // Route: /tours
-app.use("/tours", tourRouter);
+app.use("/api/tours", tourRouter);
 
-app.use("/users", userRouter);
+app.use("/api/users", userRouter);
 
 const port = 4000;
 // Start the server
